1d93842a2fee7f1a86ecd20217377df8b9ab0685
Caleb <inchristalway-tech4him@yahoo.com>
2017-09-25 20:25:17 -0600

    Use official version of unicode as a generator.

--- generate.js
+++ generate.js
@@ -1,6 +1,14 @@
 const fs = require('fs');
 const path = require('path');
-const categories = require('unicode/category');
+const unicodeDir = path.dirname(require.resolve('unicode/package.json'));
+const categoryFiles = fs.readdirSync(path.join(unicodeDir, 'category'))
+const categories = categoryFiles
+  .map((f) => (f.endsWith('.js') ? f.slice(0, -3) : undefined))
+  .reduce((list, cat) => {
+    list[cat] = require(`unicode/category/${cat}`);
+    return list;
+  }, {});
+
 const warningHeader = "// DO NOT CHANGE THIS FILE. This is an auto-generated list."
 
 const formatFile = (symbols) => (`${ warningHeader }\nmodule.exports=${JSON.stringify(symbols)}`);
